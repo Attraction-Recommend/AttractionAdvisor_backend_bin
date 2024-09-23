096a804f4a6c9729f7c81da5536a4dbbfdd7cb39
computerbg <92215707+computerbg@users.noreply.github.com>
2024-09-23 15:25:08 +0900

    first commit

---
+++ src/main/java/com/example/attractionAdvisor_bin/AttractionAdvisorBinApplication.java
@@ -0,0 +1,13 @@
+package com.example.attractionAdvisor_bin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttractionAdvisorBinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AttractionAdvisorBinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/attractionAdvisor_bin/AttractionAdvisorBinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.attractionAdvisor_bin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttractionAdvisorBinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
